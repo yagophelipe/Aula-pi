8092a010a9cea288d561aea7c65f9bfdedb3c9be
yago phelipe <yago.phelipe@escolar.ifrn.edu.br>
2026-09-01 16:05:30 -0300

    configurando classe de modelo Evento

--- src/main/java/ifrn/pi/eventos/models/Evento.java
+++ src/main/java/ifrn/pi/eventos/models/Evento.java
@@ -1,11 +1,28 @@
 package ifrn.pi.eventos.models;
 
-public class Evento {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
+public class Evento {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
 	private String nome;
 	private String local;
 	private String data;
 	private String horario;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
 
 	public String getNome() {
 		return nome;
