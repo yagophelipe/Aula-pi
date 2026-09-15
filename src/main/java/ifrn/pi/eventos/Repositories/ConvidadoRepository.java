b81ab3eb42bee4740af8c4a3383d111de5319609
yago phelipe <yago.phelipe@escolar.ifrn.edu.br>
2026-09-15 15:51:57 -0300

    Criando Lista de Convidados

---
+++ src/main/java/ifrn/pi/eventos/Repositories/ConvidadoRepository.java
@@ -0,0 +1,14 @@
+package ifrn.pi.eventos.Repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ifrn.pi.eventos.models.Convidado;
+import ifrn.pi.eventos.models.Evento;
+
+public interface ConvidadoRepository extends JpaRepository<Convidado, Long>{
+	
+	List<Convidado> findByEvento(Evento evento);
+
+}
--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import ifrn.pi.eventos.Repositories.ConvidadoRepository;
 import ifrn.pi.eventos.Repositories.EventoRepository;
+import ifrn.pi.eventos.models.Convidado;
 import ifrn.pi.eventos.models.Evento;
 
 @Controller
@@ -20,6 +22,8 @@ public class EventosController {
 
 	@Autowired
 	private EventoRepository er;
+	@Autowired
+	private ConvidadoRepository cr;
 
 	@GetMapping("/form")
 	public String form() {
@@ -59,9 +63,36 @@ public class EventosController {
 			md.setViewName("eventos/detalhes");
 			Evento evento = opt.get();
 			md.addObject("evento", evento);
+			
+			List<Convidado> convidados = cr.findByEvento(evento);
+			md.addObject("convidados", convidados);
 
 			return md;
+			
 
 		}
+	
+	@PostMapping("/{idEvento}")
+	public String salvarConvidado(@PathVariable Long idEvento, Convidado convidado) {
+		
+		System.out.println("Id do evento: " + idEvento);
+		System.out.println(convidado);
+		
+		Optional<Evento> opt = er.findById(idEvento);
+		if (opt.isEmpty()){
+			return "redirect:/eventos";
+			
+		}
+		
+		Evento evento = opt.get();
+		convidado.setEvento(evento);
+		
+		cr.save(convidado);
+		
+		return "redirect:/eventos/{idEvento}";
+		
+		
+		
+	}
 
 	}
